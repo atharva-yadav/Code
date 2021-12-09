a70ff1b38cec7a0e3ed1ee77efdd9ca8200b070e
Atharva Yadav <78128129+atharva-yadav@users.noreply.github.com>
2021-12-09 21:44:16 +0530

    Create leetcode-two-sum.cpp

---
+++ leetcode-two-sum.cpp
@@ -0,0 +1,46 @@
+// Naive Solution 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {        
+        vector<int> v;
+        for(int i = 0; i < nums.size(); i++)
+        {            
+            for(int j = i+1; j < nums.size(); j++)
+            {                   
+                if (nums[i] + nums[j] == target )
+                {
+                    v.push_back(i);
+                    v.push_back(j);
+                    return v;
+                }
+            }
+        }
+        return v;
+     }
+};
+
+// Optimized Solution
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        unordered_map <int, int> m;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(m.find(target-nums[i]) != m.end())
+            {
+                ans.push_back(m[target-nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+
+            m[nums[i]] = i;
+        }
+        return ans;
+    }
+};
+
